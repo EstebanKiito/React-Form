40a888198262d0efc5cdffc75fa5f3d48962a3a5
EstebanKiito <esteban.ortega@uc.cl>
2025-07-16 16:53:42 -0400

    ignore

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,5 @@
 function Button({ variant = "primary", children }) {
-  return (
-    <button type="submit" className={`btn btn-${variant}`}>
-      {children}
-    </button>
-  );
+  return <button className={`btn btn-${variant}`}>{children}</button>;
 }
 
 export default Button;
